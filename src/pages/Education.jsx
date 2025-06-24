aae6047f57a9667a670a575d6b9d716386b573ae
RichaBharti0603 <richab820@gmail.com>
2025-06-24 12:59:35 +0530

    Some changes

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Education.css";
+import collegeImage from "../assets/college.jpg"; // <-- adjust filename
 
 export default function Education() {
   const educationData = [
     {
       degree: "B.Tech in Computer Science",
-      institution: "Your College Name",
-      duration: "2021 - 2025",
+      institution: "Dronacharya College of Engineering, Gurgaon",
+      duration: "2023 - 2027",
       description:
         "Pursuing my undergraduate degree with a strong focus on software development, data structures, and building impactful projects.",
+      image: collegeImage, // new field
     },
   ];
 
@@ -18,6 +20,11 @@ export default function Education() {
       <div className="education-timeline">
         {educationData.map((edu, index) => (
           <div key={index} className="education-card">
+            <img
+              src={edu.image}
+              alt={`${edu.institution} logo`}
+              className="education-image"
+            />
             <h3>{edu.degree}</h3>
             <h4>{edu.institution}</h4>
             <p className="edu-duration">{edu.duration}</p>
@@ -28,3 +35,4 @@ export default function Education() {
     </div>
   );
 }
+
